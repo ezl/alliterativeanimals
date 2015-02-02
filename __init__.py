c320a1e2a50179ba82548af96da7915b8a062982
ezl <ericzliu@gmail.com>
2015-02-02 04:03:27 +0000

    make it usable

--- __init__.py
+++ __init__.py
@@ -1,11 +1,15 @@
 import random
+import os
 
-with open("adjectives.txt") as f:
+PATH = os.path.dirname(os.path.abspath(__file__))
+
+ADJECTIVES_FILE = os.path.join(PATH, 'adjectives.txt')
+with open(ADJECTIVES_FILE) as f:
     adjectives = [i.strip() for i in f.readlines()]
 
-with open("animals.txt") as f:
+ANIMALS_FILE = os.path.join(PATH, 'animals.txt')
+with open(ANIMALS_FILE) as f:
     animals = [i.strip() for i in f.readlines()]
-
 
 def get_name():
     adjective = random.choice(adjectives)
@@ -13,6 +17,7 @@ def get_name():
     animal = random.choice([i for i in animals if i[0] == first_letter])
     return "{} {}".format(adjective, animal)
 
-for i in range(100):
-    print get_name()
+if __name__ == "__main__":
+    for i in range(100):
+        print get_name()
 
